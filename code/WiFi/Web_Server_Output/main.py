bb3b4e6be54ed8e685cd7908a9d38fdf82694f95
Rui Santos <randomnerdtutorials@gmail.com>
2019-01-31 11:09:01 +0000

    Update main.py

--- code/WiFi/Web_Server_Output/main.py
+++ code/WiFi/Web_Server_Output/main.py
@@ -5,7 +5,7 @@ def web_page():
     led_state = 'checked'
   else:
     led_state = ""
-  html = """<!DOCTYPE HTML><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>
   body{max-width: 300px; margin: 0px auto;}
   .switch{position:relative;display:inline-block;width:120px;height:68px}.switch input{display:none}
   .slider{position:absolute;top:0;left:0;right:0;bottom:0;background-color:#ccc;border-radius:34px}
