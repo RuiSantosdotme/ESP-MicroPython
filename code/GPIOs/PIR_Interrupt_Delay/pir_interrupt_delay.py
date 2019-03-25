49ed5fe165b3cacac76d089604f346e0a566cb49
Rui Santos <randomnerdtutorials@gmail.com>
2019-03-25 10:29:04 +0000

    Update pir_interrupt_delay.py

--- code/GPIOs/PIR_Interrupt_Delay/pir_interrupt_delay.py
+++ code/GPIOs/PIR_Interrupt_Delay/pir_interrupt_delay.py
@@ -5,9 +5,11 @@ from time import sleep
 
 motion = False
 
-def handle_interrupt(v):
+def handle_interrupt(pin):
   global motion
   motion = True
+  global interrupt_pin
+  interrupt_pin = pin 
 
 led = Pin(12, Pin.OUT)
 pir = Pin(14, Pin.IN)
@@ -16,7 +18,7 @@ pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 
 while True:
   if motion:
-    print('Motion detected!')
+    print('Motion detected! Interrupt caused by:', interrupt_pin)
     led.value(1)
     sleep(20)
     led.value(0)
