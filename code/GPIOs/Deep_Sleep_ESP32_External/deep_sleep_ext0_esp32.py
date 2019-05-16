10ed8872cb444e8485f30b1ba62ab3e0130a0970
Rui Santos <hello@ruisantos.me>
2019-05-16 10:07:34 +0100

    ESP32 Deep Sleep External Wake Up

---
+++ code/GPIOs/Deep_Sleep_ESP32_External/deep_sleep_ext0_esp32.py
@@ -0,0 +1,18 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import machine
+import esp32
+from machine import Pin
+from time import sleep
+
+wake1 = Pin(14, mode = Pin.IN)
+
+#level parameter can be: esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
+esp32.wake_on_ext0(pin = wake1, level = esp32.WAKEUP_ANY_HIGH)
+
+#your main code goes here to perform a task
+
+print('Im awake. Going to sleep in 10 seconds')
+sleep(10)
+print('Going to sleep now')
+machine.deepsleep()
