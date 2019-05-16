c1d78a01787e53753f6c9a0316ecdf12a6c07d30
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-16 11:15:09 +0100

    GPIO 12 updated

--- code/GPIOs/Deep_Sleep_ESP32_External/deep_sleep_ext1_esp32.py
+++ code/GPIOs/Deep_Sleep_ESP32_External/deep_sleep_ext1_esp32.py
@@ -6,7 +6,7 @@ from machine import Pin
 from time import sleep
 
 wake1 = Pin(14, mode = Pin.IN)
-wake2 = Pin(27, mode = Pin.IN)
+wake2 = Pin(12, mode = Pin.IN)
 
 #level parameter can be: esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
 esp32.wake_on_ext1(pins = (wake1, wake2), level = esp32.WAKEUP_ANY_HIGH)
