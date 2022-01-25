fe0608ce4041282a6f65da55c6515cf0f02c6767
Rui Santos <randomnerdtutorials@gmail.com>
2022-01-25 09:32:43 +0000

    Update deep_sleep_ext0_esp32.py

--- code/GPIOs/Deep_Sleep_ESP32_External/deep_sleep_ext0_esp32.py
+++ code/GPIOs/Deep_Sleep_ESP32_External/deep_sleep_ext0_esp32.py
@@ -1,8 +1,8 @@
 # Complete project details at https://RandomNerdTutorials.com
 
-import machine
 import esp32
 from machine import Pin
+from machine import deepsleep
 from time import sleep
 
 wake1 = Pin(14, mode = Pin.IN)
@@ -15,4 +15,4 @@ esp32.wake_on_ext0(pin = wake1, level = esp32.WAKEUP_ANY_HIGH)
 print('Im awake. Going to sleep in 10 seconds')
 sleep(10)
 print('Going to sleep now')
-machine.deepsleep()
+deepsleep()
