c06d72d1fcd779bbf006b35910e92894fdc935c7
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-15 11:19:50 +0100

    Update main.py

--- code/MQTT/MQTT_Hello_World/ESP_1/main.py
+++ code/MQTT/MQTT_Hello_World/ESP_1/main.py
@@ -1,4 +1,4 @@
-# Complete project details at https://RandomNerdTutorials.com
+# Complete project details at https://RandomNerdTutorials.com/micropython-programming-with-esp32-and-esp8266/
 
 def sub_cb(topic, msg):
   print((topic, msg))
