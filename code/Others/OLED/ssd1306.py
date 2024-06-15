1d79a0e98e4e83234ca50fb79a25f79185b8be29
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-15 11:25:09 +0100

    Update ssd1306.py

--- code/Others/OLED/ssd1306.py
+++ code/Others/OLED/ssd1306.py
@@ -1,4 +1,4 @@
-#MicroPython SSD1306 OLED driver, I2C and SPI interfaces created by Adafruit
+# MicroPython SSD1306 OLED driver, I2C and SPI interfaces created by Adafruit
 
 import time
 import framebuf
