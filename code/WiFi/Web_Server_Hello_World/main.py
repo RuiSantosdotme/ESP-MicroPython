bfdb42b414faa165de77d3cdbee54f730fe1a26d
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-15 11:22:37 +0100

    Update main.py

--- code/WiFi/Web_Server_Hello_World/main.py
+++ code/WiFi/Web_Server_Hello_World/main.py
@@ -1,4 +1,4 @@
-# Complete project details at https://RandomNerdTutorials.com
+# Complete project details at https://RandomNerdTutorials.com/micropython-programming-with-esp32-and-esp8266/
 
 def web_page():
   html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
