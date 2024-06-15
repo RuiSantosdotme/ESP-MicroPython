ef03dc627cc2d1163a541f68e8b4f8ea96e736cb
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-15 11:22:55 +0100

    Update main.py

--- code/WiFi/Web_Server_Output/main.py
+++ code/WiFi/Web_Server_Output/main.py
@@ -1,4 +1,4 @@
-# Complete project details at https://RandomNerdTutorials.com
+# Complete project details at https://RandomNerdTutorials.com/micropython-programming-with-esp32-and-esp8266/
 
 def web_page():
   if led.value() == 1:
