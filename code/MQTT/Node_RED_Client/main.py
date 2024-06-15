5b0324a28cba86d5ccde4c19c1844654ee98404b
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-15 11:20:34 +0100

    Update main.py

--- code/MQTT/Node_RED_Client/main.py
+++ code/MQTT/Node_RED_Client/main.py
@@ -1,4 +1,4 @@
-# Complete project details at https://RandomNerdTutorials.com
+# Complete project details at https://RandomNerdTutorials.com/micropython-programming-with-esp32-and-esp8266/
 
 def read_ds_sensor():
   roms = ds_sensor.scan()
